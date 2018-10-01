ae795f942b0852b6f306e2f1e2fd96cca9a22510
Jongmyung Ha <jongmyung@stayntouch.com>
2018-10-01 10:52:25 -0400

    Update description for code/rabbitmq.rb

--- code/rabbitmq.rb
+++ code/rabbitmq.rb
@@ -1,9 +1,12 @@
 require 'bunny'
 
 class Rabbitmq
-  # Implement a method that will be the PubSub model using RabbitMQ and Bunny gem.
-  # Your method should publish guest to the queue, and subscribe from queue.
-  # And subscriber should generate and return the message as expected from the test.
+  # This exercise is to build a method that implement Publish/Subscribe model using RabbitMQ (Messaging Service) and Bunny gem (RabbitMQ Client).
+  #
+  # The test case will provide a guest name to your method. The method should subscribe to a queue, publish the provided guest to the queue, and then
+  # return a string with guest's name. (e.g.: "Received Name's Reservation")
+  #
+  # The test is pretty basic, but we are looking to see a working Pub/Sub implementation using RabbitMQ.
   #
   # Source (https://www.rabbitmq.com/getstarted.html, http://rubybunny.info/articles/getting_started.html)
   #
